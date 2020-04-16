9cc05d8c0db765776977538be9cbdc47e741cd9f
nmrodrigues <rodriguesnm24@gmail.com>
2020-04-16 16:36:39 -0500

    Added list tiles. Need to add packing list data.

--- lib/screens/packinglist.dart
+++ lib/screens/packinglist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(new PackingList());
 
@@ -24,7 +25,6 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-
   @override
   Widget build(BuildContext context) {
     final topAppBar = AppBar(
@@ -38,10 +38,44 @@ class _ListPageState extends State<ListPage> {
         )
       ],
     );
+    final makeListTile = ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),
+          child: Icon(Icons.autorenew, color: Colors.white),
+        ),
+        title: Text(
+          "Tolietries",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        trailing:
+            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30));
 
+    final makeCard = Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        child: makeListTile,
+      ),
+    );
+    final makeBody = Container(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard;
+        },
+      ),
+    );
     // TODO: implement build
     return Scaffold(
       backgroundColor: Color.fromRGBO(8, 66, 86, 1.0),
+      body: makeBody,
       appBar: topAppBar,
     );
   }
